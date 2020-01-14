b8a0eb7252e41b92393ea34a296a03376f1072b8
1479202229@qq.com <1479202229@qq.com>
2020-01-14 17:34:50 +0800

    关于svg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,9 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <span>
+      <svg-icon :icon-class="'gaojing'"/>
+    </span>
   </div>
 </template>
 
@@ -35,6 +38,9 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted() {
+    console.log(1)
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import './assets/svg/iconfont.js'
+import './assets/icons'
 
 Vue.config.productionTip = false
 
